3911f078832ae28c7585af6e301c3fd965115abd
Илья Кухта <mr.fantilo@gmail.com>
2025-01-07 03:00:27 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -18,15 +18,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
- */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ */class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let encryptedMessage = '';
+    let keyIndex = 0;
+    key = key.toLowerCase();
+    
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+
+      if (/[a-zA-Z]/.test(char)) {
+        let shift = key[keyIndex % key.length].charCodeAt(0) - 'a'.charCodeAt(0);
+        let base = char === char.toLowerCase() ? 'a'.charCodeAt(0) : 'A'.charCodeAt(0);
+        let encryptedChar = String.fromCharCode((char.charCodeAt(0) - base + shift) % 26 + base);
+        encryptedMessage += encryptedChar.toUpperCase();
+
+        keyIndex++;
+      } else {
+        encryptedMessage += char;
+      }
+    }
+
+    return this.direct ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let decryptedMessage = '';
+    let keyIndex = 0;
+    key = key.toLowerCase();
+    
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+
+      if (/[a-zA-Z]/.test(char)) {
+        let shift = key[keyIndex % key.length].charCodeAt(0) - 'a'.charCodeAt(0);
+        let base = char === char.toLowerCase() ? 'a'.charCodeAt(0) : 'A'.charCodeAt(0);
+        let decryptedChar = String.fromCharCode((char.charCodeAt(0) - base - shift + 26) % 26 + base);
+        decryptedMessage += decryptedChar.toUpperCase();
+
+        keyIndex++;
+      } else {
+        decryptedMessage += char;
+      }
+    }
+
+    return this.direct ? decryptedMessage : decryptedMessage.split('').reverse().join('');
   }
 }
 
